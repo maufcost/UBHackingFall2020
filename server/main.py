c8469d27e2c545c62fd3f3117937f0e0aaeab7da
Nate8888 <wilk.nathan@gmail.com>
2020-10-24 23:39:49 -0400

    Added Twitter API v2 & v1

--- server/main.py
+++ server/main.py
@@ -4,13 +4,63 @@ import json
 import string
 import random
 import math
+import os
 
 app = Flask(__name__)
+
+TWITTER_API = os.environ.get('TTKEY')
+TWITTER_API_secret = os.environ.get('TTSECRET')
+BEARER_TOKEN = os.environ.get('BTOKEN')
 
 def randomStringDigits(stringLength=2):
 
     lettersAndDigits = string.ascii_letters + string.digits
     return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+
+
+def get_username_info(user):
+
+    url = "https://api.twitter.com/2/users/by/username/{}?user.fields=profile_image_url".format(user)
+
+    payload = {}
+    headers = {
+      'Authorization': 'Bearer {}'.format(BEARER_TOKEN),
+      'Cookie': 'personalization_id="v1_p5/6aoRuy20KVDY7I31lDg=="; guest_id=v1%3A160359179220272467'
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload).json()
+    print(response)
+    return response
+
+#Given user handle OR id! -> returns up to 50 tweets
+def query_user_tweets(user):
+
+    url = "https://api.twitter.com/2/tweets/search/recent?query=from:{}&max_results=50&tweet.fields=created_at".format(user)
+
+    payload = {}
+    headers = {
+      'Authorization': 'Bearer {}'.format(BEARER_TOKEN),
+      'Cookie': 'personalization_id="v1_p5/6aoRuy20KVDY7I31lDg=="; guest_id=v1%3A160359179220272467'
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload).json()
+
+    return response
+
+
+def get_user_friends(user):
+    url = "https://api.twitter.com/1.1/friends/ids.json?screen_name={}".format(user)
+
+    payload = {}
+    headers = {
+      'Authorization': 'Bearer {}'.format(BEARER_TOKEN),
+      'Cookie': 'personalization_id="v1_p5/6aoRuy20KVDY7I31lDg=="; guest_id=v1%3A160359179220272467'
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload).json()
+
+    return response
+
 
 @app.route('/')
 def root():
